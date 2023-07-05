3682b04944a34f2751b2195e8048ed25f3391a71
MartinAlmaraz <malmaraz9@nwacc.edu>
2023-07-05 12:16:08 -0500

    Deleted some things

---
+++ movies/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Heder extends React.Component {
+    render() {
+        return (
+            <nav>
+                <div>
+                    <span></span>
+                </div>
+            </nav>
+        )
+    }
+}
